d4505d00143bec14b3cc47a497d49e7d6f800fe2
KHELIFI Ahmed Aziz <ahmedazizkhelifi@gmail.com>
2023-01-12 15:00:28 +0100

    exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import { default as MoviesList } from "./movies-list/movies-list"
 import { default as Card } from "./card/card"
 import { default as ImageWithLoading } from "./img-with-loading/img-with-loading"
-export { MoviesList, Card, ImageWithLoading }
+import { default as LikeDislikeButton } from "./like-dislike-button/like-dislike-button"
+import { default as DefaultButton } from "./default-button/default-button"
+export { MoviesList, Card, ImageWithLoading, LikeDislikeButton, DefaultButton }
