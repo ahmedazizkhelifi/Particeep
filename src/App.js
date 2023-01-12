7a80fbebbd1f46e610794dc59c5825f42dd815e4
KHELIFI Ahmed Aziz <ahmedazizkhelifi@gmail.com>
2023-01-12 10:59:08 +0100

    prettify

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React from 'react';
+import React from "react"
 import MoviesList from "./components/MoviesList"
-import './App.css'
+import "./App.css"
 
 class App extends React.Component {
-
   state = {
-    category: ''
+    category: ""
   }
 
   handleChange = (e) => {
-    this.setState({category: e.target.value})
+    this.setState({ category: e.target.value })
   }
 
   render() {
-  return (
-    <div className="App">
-          <select className="app-select" onChange={this.handleChange}>
-            <option id="Default"></option>
-            <option id="Comedy">Comedy</option>
-            <option id="Animation">Animation</option>
-            <option id="Thriller">Thriller</option>
-            <option id="Drame">Drame</option>
-          </select>
-   <MoviesList category={this.state.category}/>
-    </div>
-  );
+    return (
+      <div className="App">
+        <select className="app-select" onChange={this.handleChange}>
+          <option id="Default"></option>
+          <option id="Comedy">Comedy</option>
+          <option id="Animation">Animation</option>
+          <option id="Thriller">Thriller</option>
+          <option id="Drame">Drame</option>
+        </select>
+        <MoviesList category={this.state.category} />
+      </div>
+    )
+  }
 }
-}
-export default App;
+export default App
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,50 +1,49 @@
-import React from "react";
-import "./Movies.css";
-import { FaThumbsUp,FaThumbsDown } from "react-icons/fa";
+import React from "react"
+import "./Movies.css"
+import { FaThumbsUp, FaThumbsDown } from "react-icons/fa"
 const Movies = ({ handleClick, id, title, category, likes, dislikes }) => {
   return (
     <figure className="movie-container">
       <div className="oui">
         <img
+          alt="Movie Thumbnail"
           className="movie-img"
-          href=""
-          src="https://api.lorem.space/image/movie?w=150&h=220"
-        ></img>
-
+          src="https://api.lorem.space/image/movie?w=150&h=220"></img>
       </div>
       <div className="description">
         <div>
-        <h1 className="movie-title">{title}</h1>
-        <p>{category}</p>
+          <h1 className="movie-title">{title}</h1>
+          <p>{category}</p>
         </div>
         <div>
-        
           <div className="container">
-      <div className="btn-container">
-        <button>
-          <span className="material-symbols-rounded"><FaThumbsUp /> </span>
-          {likes}
-          
-        </button>
-        <i class="bi bi-hand-thumbs-up"></i>
-    
-        <button>
-          <span className="material-symbols-rounded"><FaThumbsDown /> </span>
-         {dislikes}
-        </button>
-      </div>
-    </div>
- 
+            <div className="btn-container">
+              <button>
+                <span className="material-symbols-rounded">
+                  <FaThumbsUp />{" "}
+                </span>
+                {likes}
+              </button>
+              <i class="bi bi-hand-thumbs-up"></i>
+
+              <button>
+                <span className="material-symbols-rounded">
+                  <FaThumbsDown />{" "}
+                </span>
+                {dislikes}
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-      
+
       <div className="div-movie-button-delete">
         <button className="movie-button-delete" onClick={() => handleClick(id)}>
           X
-        </button> 
-        </div>
+        </button>
+      </div>
     </figure>
-  );
-};
+  )
+}
 
-export default Movies;
+export default Movies
